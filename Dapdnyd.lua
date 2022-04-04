d1edb5969f66d0603e45e08fc36dae437d4e8667
CrystalManMan <86424080+CrystalManMan@users.noreply.github.com>
2022-04-04 00:34:54 -0400

    Update Dapdnyd.lua

--- Dapdnyd.lua
+++ Dapdnyd.lua
@@ -1,1 +1,3 @@
-game:GetService("Players").LocalPlayer:Kick("Outdated Verison : Please use the trail verison, I do not know if this verison will ban you. Key free: 6b24b180_5567_ecab")
+setclipboard("6b24b180_5567_ecab")
+wait(0.2)
+game:GetService("Players").LocalPlayer:Kick("Outdated Verison : Please use the trail verison, I do not know if this verison will ban you. Key copied to clipboard.")
