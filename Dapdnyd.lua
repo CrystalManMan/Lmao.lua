d2399c458a1cf6d02a112f013d28f5188cc7f3e8
CrystalManMan <86424080+CrystalManMan@users.noreply.github.com>
2022-04-04 18:01:01 -0400

    Delete Dapdnyd.lua

--- Dapdnyd.lua
+++
@@ -1,3 +0,0 @@
-setclipboard("6b24b180_5567_ecab")
-wait(0.2)
-game:GetService("Players").LocalPlayer:Kick("Outdated Verison : Please use the trail verison, I do not know if this verison will ban you. Key copied to clipboard.")
